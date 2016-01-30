0cccc32a63c5d5e67dcaebd49775c5fee38d7ab4
Emil Lauridsen <mine809@gmail.com>
2016-01-30 15:44:53 +0100

    Cleanup build script and update to LLVM 3.7.0

--- build.sh
+++ build.sh
@@ -1,28 +1,32 @@
 #!/bin/sh -xe
 
-gollvmdir=$(dirname "$0")/llvm
+# The version of LLVM targeted
+llvm_version="RELEASE_370"
 
-workdir=$gollvmdir/workdir
-llvmdir=$gollvmdir/../../..
-llvm_builddir=$workdir/llvm_build
+# Setup various paths
+basedir=$(dirname "$(readlink -f "$0")")
+gollvmdir=$basedir/llvm
+workdir=$basedir/workdir
+llvmdir=$workdir/src
+llvm_builddir=$workdir/build
 
-mkdir -p $llvm_builddir
-
-cmake_flags="../../../../.. $@"
+# Setup cmake flags and easy access to llvm-config and llvm-go
+cmake_flags="$llvmdir $@"
 llvm_config="$llvm_builddir/bin/llvm-config"
 llvm_go="$llvm_builddir/bin/llvm-go"
 
-if test -n "`which ninja`" ; then
-  # If Ninja is available, we can speed up the build by building only the
-  # required subset of LLVM.
-  (cd $llvm_builddir && cmake -G Ninja $cmake_flags)
-  ninja -C $llvm_builddir llvm-config llvm-go
-  llvm_components="$($llvm_go print-components)"
-  llvm_buildtargets="$($llvm_config --libs $llvm_components | sed -e 's/-l//g')"
-  ninja -C $llvm_builddir $llvm_buildtargets FileCheck
-else
-  (cd $llvm_builddir && cmake $cmake_flags)
-  make -C $llvm_builddir -j4
+# Create build dir
+mkdir -p $llvm_builddir
+
+# Fetch the correct version of the llvm source
+svn co "https://llvm.org/svn/llvm-project/llvm/tags/$llvm_version/final/" $llvmdir
+
+# Build llvm
+if [ ! -f $llvm_go ]
+then
+	(cd $llvm_builddir && cmake $cmake_flags)
+	make -C $llvm_builddir -j4
 fi
 
+# Create cgo config
 $llvm_go print-config > $gollvmdir/llvm_config.go
