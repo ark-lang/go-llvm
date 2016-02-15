634a2225dd4cba3eb2ce8a1b5c861c59503d7ffc
Emil Lauridsen <mine809@gmail.com>
2016-02-15 19:32:48 +0100

    Eliminate the need for -byollvm

--- build.sh
+++ build.sh
@@ -20,44 +20,49 @@ llvm_config="$llvm_builddir/bin/llvm-config"
 llvm_go="$llvm_builddir/bin/llvm-go"
 
 # Check if we have a compatible LLVM version installed
+use_system_llvm=0
 if [ -n `which llvm-config` ]
 then
 	system_llvm_version="`llvm-config --version`"
 	if [ "$system_llvm_version" = "$llvm_version" ]
 	then
 		echo "[ ] Using system LLVM"
-		rm $basellvm/llvm_config.go &> /dev/null
-		export CGO_CPPFLAGS="`llvm-config --cppflags`"
- 		export CGO_LDFLAGS="`llvm-config --ldflags --libs --system-libs all`"
- 		export CGO_CXXFLAGS="-std=c++11"
- 		go install -tags byollvm github.com/ark-lang/go-llvm/llvm
- 		exit
+
+ 		cat $gollvmdir/llvm_config.go.in | \
+ 			sed "s#@LLVM_CFLAGS@#`llvm-config --cppflags`#" | \
+ 			sed "s#@LLVM_LDFLAGS@#`llvm-config --ldflags --libs --system-libs all | tr '\n' ' '`#" > \
+ 			$gollvmdir/llvm_config.go
+
+ 		use_system_llvm=1
 	else
 		echo "[!] System LLVM version ($system_llvm_version) != required LLVM version ($llvm_version)"
 		
 	fi
 fi
 
-echo "[ ] Building LLVM from source"
-
-# Create build dir
-mkdir -p $llvm_builddir
-
-# Fetch the correct version of the llvm source
-echo "[ ] Fetching LLVM source tree"
-svn co --quiet "https://llvm.org/svn/llvm-project/llvm/tags/$llvm_svn_tag/final/" $llvmdir
-
-# Build llvm
-if [ ! -f $llvm_go ]
+if [ "$use_system_llvm" = "0" ]
 then
-	echo "[ ] Building LLVM"
-	(cd $llvm_builddir && cmake $cmake_flags)
-	make -C $llvm_builddir -j4
-fi
+	echo "[ ] Building LLVM from source"
 
-# Create cgo config
-echo "[ ] Creating CGO config"
-$llvm_go print-config > $gollvmdir/llvm_config.go
+	# Create build dir
+	mkdir -p $llvm_builddir
+
+	# Fetch the correct version of the llvm source
+	echo "[ ] Fetching LLVM source tree"
+	svn co --quiet "https://llvm.org/svn/llvm-project/llvm/tags/$llvm_svn_tag/final/" $llvmdir
+
+	# Build llvm
+	if [ ! -f $llvm_go ]
+	then
+		echo "[ ] Building LLVM"
+		(cd $llvm_builddir && cmake $cmake_flags)
+		make -C $llvm_builddir -j4
+	fi
+
+	# Create cgo config
+	echo "[ ] Creating CGO config"
+	$llvm_go print-config > $gollvmdir/llvm_config.go
+fi
 
 # Install binding
 echo "[ ] Installing LLVM bindings"
